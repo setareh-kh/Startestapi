c88dba66dfcf539431c1d463dcd4ff3493a2de17
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2023-11-06 16:47:34 +0330

    change private array in UserController from my Institute

--- Controllers/userController.cs
+++ Controllers/userController.cs
@@ -8,7 +8,7 @@ namespace Controllers
     {
         private static readonly string[] Summaries = new[]
     {
-        "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Love", "My", "Master", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     }
