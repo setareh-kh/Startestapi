cfd44ca1fc8e8d420fc4e43533ecdd87c5816c16
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2024-01-01 13:06:00 +0330

    swap cool to Esfhan in weathercontroller in my home

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Norabd", "Tehran", "shiraz", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Norabd", "Tehran", "shiraz", "Esfahan", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
